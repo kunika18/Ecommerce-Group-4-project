ac667b02ebb6f9510cf607ab11d1601bcf37d067
Kunika Singh <singhkunika18@gmail.com>
2024-08-03 21:16:42 +0530

    updating existing files

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
-// import {BrowserRouter, Routes, Route} from 'react-router-dom';
+
+ 
+import Shop from './Pages/Shop'; 
 
 function App() {
   return (
-    <div >
-      {/* <BrowserRouter> */}
-      <Navbar/>
-      {/* </BrowserRouter> */}
+    <div>
+      <Navbar />
+      
+      <Shop/>
+
     </div>
   );
 }
