d07e2a764a179436a07159f546476a56cb722b3c
Navneet-kaur7 <navneetgill1234@gmail.com>
2024-08-03 17:20:57 +0530

    Item created

--- src/App.js
+++ src/App.js
@@ -11,13 +11,15 @@ function App() {
   return (
     <div >
       <BrowserRouter> 
-      <Navbar/>
+      { <Navbar/> }
       <Routes>
-        <Route path='/' element={<Shop/>}/>
+        {/* <Route path='/' element={<Shop/>}/> */}
         <Route path='/mens' element={<ShopCategory banner={men_banner} category="men"/>}/>
         <Route path='/womens' element={<ShopCategory banner={women_banner} category="women"/>}/>
         <Route path='/kids' element={<ShopCategory banner={kid_banner} category="kid"/>}/>
         {/* <Route path="/product" element={<Product/>}> */}
+        {/* <Route path=":productId" element={<Product/>}></Route>
+        <Route path='/cart' element={<Cart/>}/> */}
       </Routes>
      </BrowserRouter>
     </div>
