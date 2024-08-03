ae53e8015b3133602a21932edab35db2a4289bdb
Navneet-kaur7 <navneetgill1234@gmail.com>
2024-08-03 16:20:33 +0530

    update category

--- src/Pages/ShopCategory.jsx
+++ src/Pages/ShopCategory.jsx
@@ -1,4 +1,35 @@
-import React from 'react'
+import React, { useContext } from 'react'
 
 import './CSS/ShopCategory.css'
-const ShopCategory =()
+import { ShopContext } from '../Context/ShopContext'
+import dropdown_icon from '../Components/Assets/dropdown_icon.png'
+import Item from '../Components/Assets/'
+
+const ShopCategory =()=>{
+    const {all_product} = useContext(ShopContext);
+    return(
+        <div className='shop-category'>
+            <img src="{props.banner}" alt="" />
+            <div className='shopcategory-indexSort'>
+                <p>
+                    <span>Showing 1-12</span>out of 36 products
+                </p>
+                <div className='shopcategory-sort'>
+                    Sort by <img src="" alt=""/>
+                </div>
+            </div>
+            <div className="shopcategory-products">
+                {all_product.map=((item,i)=>{
+                    if(prop.category==item.category){
+                        return <Item key={i} id={item.id} name={item.name}  image={item.image} new_price={item.new_price} old_price={item.old_price}/>
+                    }else{
+                        return null;
+                    }
+
+                })}
+            </div>
+        </div>
+    
+    )
+}
+export default ShopCategory
